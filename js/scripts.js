9f86da3d66788f98994a54068cb35045f1d7078b
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-03-01 21:03:13 -0800

    Added a loop
    Added a loop for the pokemonList
    It will return all the items in the list, and include a message "wow, that's big" if the item height is larger than or equal to 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 let pokemonList = [
-      { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-      { name: 'Charmander', height: 6, types: 'fire'},
-      { name: 'Squirtle', height: 5, types: 'water'},
+      { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+      { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+      { name: 'Squirtle', height: 1, types: ['water']},
   ];
+
+for (let i=0; i<pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")" );
+if (pokemonList[i].height >= 1.5) {
+    document.write("Wow, that's big!!")
+    }
+}

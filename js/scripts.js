9aa7007bc742435f661b0d35043cbacff62d4452
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-03-17 18:09:41 -0700

    Updated JS
    Included template literals to make the code more readable.
    I also declared a variable to reference within the loop, to also make the code more readable

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
-let pokemonList = [
+const pokemonList = [
       { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
       { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
       { name: 'Squirtle', height: 1, types: ['water']},
   ];
 
-
-const pokemon = pokemonList[i];
-for (let i=0; i<pokemon.length; i++) {
-  document.write(pokemon.name + " (height: " + pokemon.height + ")" );
+for (let i = 0; i<pokemonList.length; i++) {
+  const pokemon = pokemonList[i];
+  document.write(`${pokemon.name} (height: ${pokemon.height})`);
   if (pokemon.height >= 1.5) {
-    document.write("Wow, that's big!!")
+    document.write(" - Wow, that's big!!")
       }
+      document.write("<br>");
   }

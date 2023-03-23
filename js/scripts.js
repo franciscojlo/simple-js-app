f99dde4274632e0cda8895dd1249b63b5f2d8002
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-03-22 20:05:59 -0700

    replaced for loops with forEach()
    wrapped the entire loop in an IIFE .
    The IIFE returned the array items with the new functions implemented.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,28 @@
-const pokemonList = [
-      { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-      { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-      { name: 'Squirtle', height: 1, types: ['water']},
-  ];
+const pokemonRepository = (function() {
+  const pokemonList = [
+          { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+          { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
+          { name: 'Squirtle', height: 1, types: ['water']},
+      ];
 
-for (let i = 0; i<pokemonList.length; i++) {
-  const pokemon = pokemonList[i];
-  document.write(`${pokemon.name} (height: ${pokemon.height})`);
-  if (pokemon.height >= 1.5) {
-    document.write(" - Wow, that's big!!")
-      }
-      document.write("<br>");
+  function getAll() {
+    return pokemonList;
   }
+
+  function add(pokemon) {
+    pokemonList.push(pokemon); 
+  } 
+
+  return { 
+    getAll: getAll,
+    add: add,
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon) {
+        document.write(`${pokemon.name} (height: ${pokemon.height})`);
+        if (pokemon.height >= 1.5) {
+          document.write(" - Wow, that's big!");
+        }
+        document.write('<br>');
+})();

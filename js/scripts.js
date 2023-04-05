9292c80851c84455ffce29e184f08f97af87d827
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-04-04 21:33:48 -0700

    added styling for the buttons
    created a simple set of properties to display the buttons for the pokedex.

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ const pokemonRepository = (function() {
           { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
           { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
           { name: 'Squirtle', height: 1, types: ['water']},
-      ];
+  ];
 
   function getAll() {
     return pokemonList;
@@ -13,16 +13,36 @@ const pokemonRepository = (function() {
     pokemonList.push(pokemon); 
   } 
 
+  function addListItem(pokemon) {
+        const pokedex = document.querySelector('.pokedex');
+        const listItem = document.createElement('li');
+        const pokebutton = document.createElement('button');
+        pokebutton.innerText = `${pokemon.name} (height: ${pokemon.height})`;
+        pokebutton.classList.add('pokebutton');
+        listItem.appendChild(pokebutton);
+        pokedex.appendChild(listItem);
+        if (pokemon.height >= 1.5) {
+           pokebutton.innerText += " - Wow, that's big!";
+          }        
+          const lineBreak = document.createElement('br');
+          pokedex.appendChild(listItem);  
+
+          pokebutton.addEventListener('click', function() {
+            showDetails(pokemon);
+          })
+        }
+
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
+
   return { 
     getAll: getAll,
     add: add,
+    addListItem: addListItem
   };
 })();
 
 pokemonRepository.getAll().forEach(function(pokemon) {
-        document.write(`${pokemon.name} (height: ${pokemon.height})`);
-        if (pokemon.height >= 1.5) {
-          document.write(" - Wow, that's big!");
-        }
-        document.write('<br>');
-})();
+  pokemonRepository.addListItem(pokemon);
+});

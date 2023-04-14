080e904f0cc9edfebda9be872ad06b3d4dc911e4
franciscojlo <120750470+franciscojlo@users.noreply.github.com>
2023-04-13 20:20:08 -0700

    Addedd polyfils for promises and fetch
    Will display all Pokemon in the api. When clicking, it will load the Pokemon clicked/returned.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,5 @@
 const pokemonRepository = (function() {
-  const pokemonList = [
-          { name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-          { name: 'Charizard', height: 1.7, types: ['fire', 'flying']},
-          { name: 'Squirtle', height: 1, types: ['water']},
-  ];
+  const pokemonList = [];
 
   function getAll() {
     return pokemonList;
@@ -14,35 +10,77 @@ const pokemonRepository = (function() {
   } 
 
   function addListItem(pokemon) {
-        const pokedex = document.querySelector('.pokedex');
-        const listItem = document.createElement('li');
-        const pokebutton = document.createElement('button');
-        pokebutton.innerText = `${pokemon.name} (height: ${pokemon.height})`;
-        pokebutton.classList.add('pokebutton');
-        listItem.appendChild(pokebutton);
-        pokedex.appendChild(listItem);
-        if (pokemon.height >= 1.5) {
-           pokebutton.innerText += " - Wow, that's big!";
-          }        
-          const lineBreak = document.createElement('br');
-          pokedex.appendChild(listItem);  
+    const pokedex = document.querySelector('.pokedex');
+    const listItem = document.createElement('li');
+    const pokebutton = document.createElement('button');
+    pokebutton.innerText = `${pokemon.name} (height: ${pokemon.height})`;
+    pokebutton.classList.add('pokebutton');
+    listItem.appendChild(pokebutton);
+    if (pokemon.height >= 1.5) { 
+      pokebutton.innerText += " - Wow, that's big!";
+    }        
+    const lineBreak = document.createElement('br');
+    listItem.appendChild(lineBreak);
+    pokedex.appendChild(listItem);  
 
-          pokebutton.addEventListener('click', function() {
-            showDetails(pokemon);
-          })
-        }
+    pokebutton.addEventListener('click', function() {
+      showDetails(pokemon);
+    })
+  }
 
   function showDetails(pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function(details) {
+      console.log(details);
+      pokemon.imgUrl = details.sprites.front_default;
+      pokemon.height = details.height;
+      const button = document.querySelector(`.pokebutton[data-name="${pokemon.name}"]`);
+      button.innerText = `${pokemon.name} (height: ${pokemon.height})`;
+      if (pokemon.height >= 1.5) {
+        button.innerText += " - Wow, that's big!";
+      }
+    });
+  }
+
+  function loadList() {
+    return fetch('https://pokeapi.co/api/v2/pokemon/')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+        json.results.forEach(function(item) {
+          const pokemon = {
+            name: item.name,
+            detailsUrl: item.url
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
+  }
+
+    function loadDetails(pokemon) {
+    return fetch(pokemon.detailsUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .catch(function(e) {
+        console.error(e);
+      });
   }
 
   return { 
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
